38fdce7d44c662dfbadb5dcd877b61d7465fb880
Yoon321 <tkdwls734@gmail.com>
2022-07-07 12:48:29 +0900

    test commit

---
+++ ebtest/src/main/java/com/example/ebtest/EbtestApplication.java
@@ -0,0 +1,13 @@
+package com.example.ebtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbtestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EbtestApplication.class, args);
+    }
+
+}
---
+++ ebtest/src/test/java/com/example/ebtest/EbtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ebtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbtestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
